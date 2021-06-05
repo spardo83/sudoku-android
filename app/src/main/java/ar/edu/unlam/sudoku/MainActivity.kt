5703429e93c26e1c4c2d68182881ef3d30412eea
brtab <brtabarez@alumno.unlam.edu.ar>
2021-06-05 17:01:07 -0300

    modificaciones esteticas en el activity main y primer acercamiento a navegar entre activities

--- app/src/main/java/ar/edu/unlam/sudoku/MainActivity.kt
+++ app/src/main/java/ar/edu/unlam/sudoku/MainActivity.kt
@@ -2,10 +2,17 @@ package ar.edu.unlam.sudoku
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ar.edu.unlam.sudoku.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.nuevoJuego.setOnClickListener {
+            SudokuActivity()
+        }
     }
 }
