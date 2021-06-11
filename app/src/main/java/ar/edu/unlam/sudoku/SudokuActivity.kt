64d14ac2f7a732deb09a91cefbc2fae5a79ce248
brtab <brtabarez@alumno.unlam.edu.ar>
2021-06-11 10:22:04 -0300

    corrijo error de ActivityBinding en la vista sudoku

--- app/src/main/java/ar/edu/unlam/sudoku/SudokuActivity.kt
+++ app/src/main/java/ar/edu/unlam/sudoku/SudokuActivity.kt
@@ -3,12 +3,13 @@ package ar.edu.unlam.sudoku
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ar.edu.unlam.sudoku.databinding.ActivityMainBinding
+import ar.edu.unlam.sudoku.databinding.ActivitySudokuBinding
 
 class SudokuActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding:ActivitySudokuBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivitySudokuBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
